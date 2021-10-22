e33dc6eca56cc1a486e091e9f5557ef282a5b467
Natalia Tavares <the-nat@hotmail.com>
2021-10-22 12:13:17 -0300

    inicio do projeto

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    println("<???> Opa, eae")
+    println("<Rogério> De boas?")
+    println("<???> Suaves")
+    println("<Rogéiro> Qual o seu nome?")
+    println("<???> Meu nome é Henrique")
+    println("<Rogério> Opa, prazer em conhecê-lo Henrique")
+    println("<Henrique> O prazer é todo meu")
+}
